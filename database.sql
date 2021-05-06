57dde45c0e192ba97503e75eac64381d9beaf4dc
Mkefss <Mauricio.291202@gmail.com>
2021-05-05 20:48:03 -0500

    ingredientes

---
+++ database.sql
@@ -0,0 +1,15 @@
+-- Creamos la base de datos y cambiamos a eso
+create database if not exists ingredientes;
+use ingredientes;
+
+-- Creamos la tabla de la base de datos
+create or replace table lista (
+	id int not null auto_increment,
+	ingrediente varchar(40) not null,
+	imagen varchar(30) not null
+);
+
+-- Ahora si insertamos las cosas
+insert into lista values 
+	("sal", "5-05-2021_sal.png"),
+	("arroz", "5-05-2021");
