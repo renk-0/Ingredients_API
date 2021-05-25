0f598df999ad2bf91df4fe41fe2fa5c28a6ec9a8
Mkefss <Mauricio.291202@gmail.com>
2021-05-24 22:25:21 -0500

    yeas

--- index.php
+++ index.php
@@ -11,17 +11,7 @@ function badResponse() {
 
 if(count($query) < 1)
 	badResponse();
-if($query[0] === "ingredientes") {
-	$conn = new mysqli(null, "***", "***", "ingredientes");
-	if($conn) {
-		header("Content-type: application/json");
-		$result = $conn->query("select * from lista;");
-		$res = $result->fetch_all();
-		echo json_encode($res);
-		$result->close();
-		$conn->close();
-	}
-} else if($query[0] === "imagen" && isset($_GET["n"])) {
+if($query[0] === "imagen" && isset($_GET["n"])) {
 	$name = $_GET["n"];
 	header("Content-type: image/jpg");
 	readfile("./imagenes/$name");
